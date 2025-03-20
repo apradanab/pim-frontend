f8fbde6764de8d4302cc325a97b37f9f82ae000d
apradanab <apradanab@gmail.com>
2025-03-20 12:40:58 +0100

    Refactored project structure and updated base configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { IntroSectionComponent } from './components/intro-section/intro-section.component';
 
 @Component({
   selector: 'pim-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, IntroSectionComponent],
+  imports: [RouterOutlet],
   template: `
-    <pim-header></pim-header>
-    <pim-intro-section></pim-intro-section>
     <router-outlet />
   `,
-  styles: [``],
+  styles: [],
 })
 export class AppComponent {
 
