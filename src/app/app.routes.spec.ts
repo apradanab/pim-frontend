33d031ffe8f0016b0bdd3cf6963a3b08bb4ae271
apradanab <apradanab@gmail.com>
2025-03-20 16:24:34 +0100

    Add test for app routes

---
+++ src/app/app.routes.spec.ts
@@ -0,0 +1,18 @@
+import { routes } from './app.routes';
+
+describe('App Routes', () => {
+  it('should have a default redirect to /home', () => {
+    const defaultRoute = routes.find(route => route.path === '');
+    expect(defaultRoute).toBeDefined();
+    expect(defaultRoute?.redirectTo).toBe('home');
+  });
+
+  it('should lazy load HomeComponent on /home', async () => {
+    const homeRoute = routes.find(route => route.path === 'home');
+    expect(homeRoute).toBeDefined();
+    expect(homeRoute?.loadComponent).toBeDefined();
+
+    const homeComponent = await homeRoute!.loadComponent!();
+    expect(homeComponent).toBeDefined();
+  });
+});
