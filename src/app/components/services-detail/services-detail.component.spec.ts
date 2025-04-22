6cf5933156fc80a64a0668c0639a8e9bd57d5903
apradanab <apradanab@gmail.com>
2025-04-22 21:59:19 +0200

    fix(test): add HTTP providers to ServicesDetailComponent test

--- src/app/components/services-detail/services-detail.component.spec.ts
+++ src/app/components/services-detail/services-detail.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ServicesDetailComponent } from './services-detail.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { StateService } from '../../services/state.service';
+import { provideHttpClient } from '@angular/common/http';
 
-describe('ServiceDetailComponent', () => {
+describe('ServicesDetailComponent', () => {
   let component: ServicesDetailComponent;
   let fixture: ComponentFixture<ServicesDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ServicesDetailComponent]
-    })
-    .compileComponents();
+      imports: [ServicesDetailComponent],
+      providers: [
+        StateService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ServicesDetailComponent);
     component = fixture.componentInstance;
