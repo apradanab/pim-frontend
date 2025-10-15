62c344aba08c51842e5c0891f704553de6de65a1
apradanab <apradanab@gmail.com>
2025-10-15 21:53:44 +0200

    chore(environment): update development URLs for API and CDN

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://jft3i0ss2i.execute-api.eu-west-1.amazonaws.com/prod'
+  apiUrl: 'https://jft3i0ss2i.execute-api.eu-west-1.amazonaws.com/prod',
+  cdnUrl: 'https://d3g74f23lfqw7t.cloudfront.net'
 };
