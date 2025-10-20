514a0fb4cf1a0a382f17cc15e79170b127b55a51
apradanab <apradanab@gmail.com>
2025-10-20 23:17:32 +0200

    refactor(core): move appointments service to repos folder

--- src/app/core/services/repos/appointments.repo.service.ts
+++ src/app/core/services/repos/appointments.repo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
-import { Appointment } from '../../models/appointment.model';
+import { environment } from '../../../../environments/environment.development';
+import { Appointment } from '../../../models/appointment.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
