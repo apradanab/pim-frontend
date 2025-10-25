9409d5b50f17a60acd9046aa86bba730296096ae
apradanab <apradanab@gmail.com>
2025-10-25 22:32:34 +0200

    feat: add user profile view

---
+++ src/app/features/user-profile-view/user-profile-view.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+// import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+// import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
+
+@Component({
+  selector: 'pim-user-profile-view',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent,
+    //  ProfileHeaderComponent, AppointmentsListComponent
+    ],
+  template: `
+  <pim-header></pim-header>
+  <!-- <pim-profile-header></pim-profile-header>
+  <pim-appointments-list></pim-appointments-list> -->
+  <pim-footer></pim-footer>
+  `,
+  styles: ``
+})
+export default class UserProfileViewComponent {
+
+}
