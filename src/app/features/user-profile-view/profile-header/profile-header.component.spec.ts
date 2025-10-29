9e138b5e809a2e7735f6467308cbb0c806592dc1
apradanab <apradanab@gmail.com>
2025-10-29 20:47:26 +0100

    feat(profile): add ProfileHeader component for user info and navigation

---
+++ src/app/features/user-profile-view/profile-header/profile-header.component.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileHeaderComponent } from './profile-header.component';
+import { Router } from '@angular/router';
+import { AuthStateService } from '../../../core/services/states/auth.state.service';
+import { UsersStateService } from '../../../core/services/states/users.state.service';
+
+describe('ProfileHeaderComponent', () => {
+  let component: ProfileHeaderComponent;
+  let fixture: ComponentFixture<ProfileHeaderComponent>;
+  let router: Router;
+  let mockUsersStateService: jasmine.SpyObj<UsersStateService>;
+  let mockAuthStateService: jasmine.SpyObj<AuthStateService>;
+
+  beforeEach(async () => {
+    mockUsersStateService = jasmine.createSpyObj('UsersStateService', ['usersState']);
+    mockAuthStateService = jasmine.createSpyObj('AuthStateService', ['logout']);
+    router = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      imports: [ProfileHeaderComponent],
+      providers: [
+        { provide: UsersStateService, useValue: mockUsersStateService },
+        { provide: AuthStateService, useValue: mockAuthStateService },
+        { provide: Router, useValue: router }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProfileHeaderComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call router.navigate with /horarios when navigateToSchedule is called', () => {
+    component.navigateToSchedule();
+    expect(router.navigate).toHaveBeenCalledWith(['/horarios']);
+  });
+
+  it('should call logout and navigate to /home when logout is called', () => {
+    component.logout();
+    expect(mockAuthStateService.logout).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/home']);
+  })
+});
---
+++ src/app/features/user-profile-view/profile-header/profile-header.component.ts
@@ -0,0 +1,157 @@
+import { Component, computed, inject } from '@angular/core';
+import { UsersStateService } from '../../../core/services/states/users.state.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import { Router } from '@angular/router';
+import { AuthStateService } from '../../../core/services/states/auth.state.service';
+
+@Component({
+  selector: 'pim-profile-header',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  template: `
+  <div class="profile-header">
+    <div class="background"></div>
+    <div class="profile-card">
+      @if (currentUser()) {
+        <div class="avatar-area">
+          @if (currentUser()?.avatar?.url; as avatarUrl) {
+            <img [src]="avatarUrl" alt="Avatar de usuario" class="avatar-img"/>
+          } @else {
+            <fa-icon [icon]="faUserCircle" class="default-icon"></fa-icon>
+          }
+        </div>
+
+        <div class="info-area">
+          <h2> {{ currentUser()?.name || currentUser()?.email || 'Usuario' }}</h2>
+          <p> {{ currentUser()?.email }} </p>
+          <p> {{ currentUser()?.createdAt }} </p>
+        </div>
+
+        <div class="button-area">
+          <button class="schedule-button" (click)="navigateToSchedule()">Reservar cita</button>
+          <button class="edit-button">Editar perfil</button>
+          <button class="logout-button" (click)="logout()">Cerrar sesión</button>
+        </div>
+      }
+    </div>
+  </div>
+  `,
+  styles: `
+  .profile-header {
+    margin: 0 8.4vw;
+    margin-top: 1.3rem;
+  }
+
+  .background {
+    height: 110px;
+    background-color: #f9f9f9;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 12px;
+    background-image:
+      repeating-linear-gradient(90deg, #f4f2ed 0, #f4f2ed 1px, transparent 1px, transparent 100%),
+      repeating-linear-gradient(0deg, #f4f2ed 0, #f4f2ed 1px, transparent 1px, transparent 100%);
+    background-size: calc(100% / 14) calc(100% / 2);
+  }
+
+  .profile-card {
+    display: flex;
+    align-items: center;
+    padding: 20px 15px 0 15px;
+    margin-bottom: 20px;
+  }
+
+  .avatar-area {
+    position: relative;
+    bottom: 5vw;
+    left: 10vw;
+  }
+
+  .avatar-img, .default-icon {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    object-fit: cover;
+
+    color: #514545;
+  }
+
+  .default-icon {
+    width: 70px;
+    height: 70px;
+    color: #1bbdbf;
+    font-size: 70px;
+    position: relative;
+    bottom: 2px;
+  }
+
+  .info-area {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    font-family: 'Carlito', sans-serif;
+  }
+
+  .button-area {
+    display: flex;
+  }
+
+  .schedule-button {
+    padding: 10px 15px;
+    background-color: #eafa74ff;
+    border: 1px solid #c3d060ff;
+    border-radius: 0.6rem;
+  }
+
+  .schedule-button:hover {
+    background-color: #c3d060ff;
+  }
+
+  .edit-button {
+    padding: 10px 15px;
+    background-color: #fbc3b4ff;
+    border: 1px solid #ca9f93ff;
+    border-radius: 0.6rem;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  .edit-button:hover {
+    background-color: #ca9f93ff;
+  }
+
+  .logout-button {
+    background-color: #f5f5f5;
+    border: 1px solid #ddd;
+    padding: 10px 15px;
+    border-radius: 0.6rem;
+    cursor: pointer;
+    transition: all 0.2s;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .logout-button:hover {
+    background-color: #ddd;
+  }
+
+  `
+})
+export class ProfileHeaderComponent {
+  private readonly usersState = inject(UsersStateService);
+  private readonly authService = inject(AuthStateService);
+  private readonly router = inject(Router);
+
+  faUserCircle = faUserCircle;
+
+  currentUser = computed(() => this.usersState.usersState().currentUser);
+
+  navigateToSchedule() {
+    this.router.navigate(['/horarios']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
+}
