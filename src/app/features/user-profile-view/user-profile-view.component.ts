60cf5c1efeca20b0821fbfc83f292f8b9a6f7df7
apradanab <apradanab@gmail.com>
2025-11-07 23:55:21 +0100

    feat(user-profile-view): integrate ProfileHeaderComponent and AppointmentsListComponent

--- src/app/features/user-profile-view/user-profile-view.component.ts
+++ src/app/features/user-profile-view/user-profile-view.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
-// import { ProfileHeaderComponent } from './profile-header/profile-header.component';
-// import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
 
 @Component({
   selector: 'pim-user-profile-view',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,
-    //  ProfileHeaderComponent, AppointmentsListComponent
-    ],
+  imports: [HeaderComponent, FooterComponent, ProfileHeaderComponent, AppointmentsListComponent],
   template: `
-  <pim-header></pim-header>
-  <!-- <pim-profile-header></pim-profile-header>
-  <pim-appointments-list></pim-appointments-list> -->
-  <pim-footer></pim-footer>
+  <div>
+    <pim-header></pim-header>
+    <pim-profile-header></pim-profile-header>
+    <pim-appointments-list></pim-appointments-list>
+    <pim-footer></pim-footer>
+  </div>
   `,
   styles: ``
 })
