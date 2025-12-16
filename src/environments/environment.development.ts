153864c576dab6846ad6c0e4e87333d5f0821e12
apradanab <apradanab@gmail.com>
2025-12-16 19:28:50 +0100

    chore: fix lint and formatting issues with eslint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://jft3i0ss2i.execute-api.eu-west-1.amazonaws.com/prod',
-  cdnUrl: 'https://d3g74f23lfqw7t.cloudfront.net'
+  cdnUrl: 'https://d3g74f23lfqw7t.cloudfront.net',
+  mediaApiKey: 'MEDIA_API_KEY_SECRET',
 };
